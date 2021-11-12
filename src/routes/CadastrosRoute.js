53f46015cc8d802bc9631dbfc8395aed99037d3c
leonir12 <83736327+leonir12@users.noreply.github.com>
2021-11-11 22:36:01 -0400

    sequelize completo

--- src/routes/CadastrosRoute.js
+++ src/routes/CadastrosRoute.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const CadastrosController = require('../controllers/CadastrosController')
 const router = Router();
 
 
 router.get('/cadastros', CadastrosController.pegaTodosOsCadastros);
-
+router.get('/cadastros/:id',CadastrosController.pegaUmCadastro);
+router.post('/cadastros', CadastrosController.criaCadastro);
+router.put('/cadastros/:id', CadastrosController.atualizaCadastro);
+router.delete('/cadastros/:id', CadastrosController.apagarCadastro);
 
 
 module.exports = router;
