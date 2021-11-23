ac0621a0c67b90f265359d7380293ae976857ab4
unknown <leonirmiller@gmail.com>
2021-11-23 09:47:01 -0400

    dia 21-11

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 const express = require("express")
 const app = express()
 import path from 'path'
+const multer = require('multer');
+
+/**
+ * Cria uma instância do middleware configurada
+ * destination: lida com o destino
+ * filenane: permite definir o nome do arquivo gravado
+ */
+ const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        //Primeiro parâmetro = erro
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        //Primeiro parâmetro = erro
+        //Salvando com name do input e data atual
+        /* cb(null, file.fieldname + '-' + Date.now()) */
+
+        //Salvando com a mesma extensão do arquivo
+        /* cb(null,`${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`) */
+
+        //Salvando com o mesmo nome do arquivo
+        cb(null, file.originalname);
+    }
+});
+
+// utiliza a storage para configurar a instância do multer
+const upload = multer({ storage });
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -18,6 +45,7 @@ app.listen(PORTA, function () {
 
 
 app.use('/publico', express.static(__dirname + '/src/publico'));
+app.use('/uploads', express.static('./uploads'));
 app.use('/bsjs', express.static('./node_modules/bootstrap/dist/js'));
 app.use('/popperjs', express.static('./node_modules/@popperjs/core/dist/umd'));
 app.use('/jquery', express.static('./node_modules/jquery/dist'));
